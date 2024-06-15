6e19839ef59814eb3ea1b2a688d5846d8d681a97
Volodymyr <vovoset82@gmail.com>
2024-06-15 17:30:16 +0300

    Change in API condition

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,23 +2,34 @@ import { db } from "@/lib/db";
 import { Prisma } from "@prisma/client";
 
 export async function saveJSON_WORDS(words: any, userId: any) {
-    const listOfWords = words as Prisma.WordsGetPayload<{}>[];    
-    listOfWords.forEach(async (word) => {
-      const data = await db.words.findFirst({
-        where: {
-          userId: userId,
-          russian: {
-            contains: word.russian,
+  const listOfWords = words as Prisma.WordsGetPayload<{}>[];
+  listOfWords.forEach(async (word) => {
+    const data = await db.words.findFirst({
+      where: {
+        userId: userId,
+        OR: [
+          {
+            russian :{
+              contains: word.russian
+            }
           },
+          {
+            german :{
+              contains: word.german
+            }
+          }
+
+        ] 
+          
+      },
+    });
+    if (!data) {
+      await db.words.create({
+        data: {
+          ...word,
+          userId,
         },
       });
-      if (!data) {
-        await db.words.create({
-          data: {
-            ...word,
-            userId,
-          },
-        });
-      }
-    });
-  }
+    }
+  });
+}
