a49441d80dc3e4616b05fcb1fdabc1f4f5e7d22d
Volodymyr <vovoset82@gmail.com>
2024-06-16 16:29:21 +0300

    chng

--- src/app/api/words/route.ts
+++ src/app/api/words/route.ts
@@ -1,6 +1,47 @@
-import { saveJSON_WORDS } from "@/actions";
+import { db } from "@/lib/db";
+import { Prisma } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+export async function saveJSON_WORDS(words: any, userId: any) {  
+  const listOfWords = words as Prisma.WordsGetPayload<{}>[];
+  listOfWords.forEach(async (word) => {
+    console.log('Getting data from data base')
+    try {
+
+      const data = await db.words.findFirst({
+        where: {
+          userId: userId,
+          OR: [
+            {
+              russian: {
+                contains: word.russian
+              }
+            },
+            {
+              german: {
+                contains: word.german
+              }
+            }
+
+          ]
+
+        },
+      });
+
+      console.log('Word has been already in  data base ', data)
+      if (!data) {
+        await db.words.create({
+          data: {
+            ...word,
+            userId,
+          },
+        });
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  });
+}
 
 
 export async function POST(req: Request) {
