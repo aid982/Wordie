69571f3a4374571d01eb28f8a3ccd9c18d62c011
Volodymyr <vovoset82@gmail.com>
2024-06-16 14:23:07 +0300

    logging

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,33 +5,39 @@ export async function saveJSON_WORDS(words: any, userId: any) {
   const listOfWords = words as Prisma.WordsGetPayload<{}>[];
   listOfWords.forEach(async (word) => {
     console.log('Getting data from data base')
-    const data = await db.words.findFirst({
-      where: {
-        userId: userId,
-        OR: [
-          {
-            russian :{
-              contains: word.russian
-            }
-          },
-          {
-            german :{
-              contains: word.german
-            }
-          }
+    try {
 
-        ] 
-          
-      },
-    });
-    console.log('Word has been already in  data base ',data)
-    if (!data) {
-      await db.words.create({
-        data: {
-          ...word,
-          userId,
+      const data = await db.words.findFirst({
+        where: {
+          userId: userId,
+          OR: [
+            {
+              russian: {
+                contains: word.russian
+              }
+            },
+            {
+              german: {
+                contains: word.german
+              }
+            }
+
+          ]
+
         },
       });
+
+      console.log('Word has been already in  data base ', data)
+      if (!data) {
+        await db.words.create({
+          data: {
+            ...word,
+            userId,
+          },
+        });
+      }
+    } catch (error) {
+      console.error(error)
     }
   });
 }
